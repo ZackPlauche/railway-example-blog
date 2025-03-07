99bd0de957483ccefe63b08008d5a9369337db75
Zack Plauche <zackplauche@gmail.com>
2025-03-07 11:35:45 +0000

    Remove email stuff

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -20,12 +20,3 @@ X_FRAME_OPTIONS = 'DENY'
 
 # Static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
